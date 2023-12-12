d13524fe096c7d73ff5039723af8b9782498bf61
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-12-12 23:47:29 +0500

    created array

---
+++ app.js
@@ -0,0 +1,116 @@
+let studenta = ["moosa","khan","jalil","eijaz","noman"];
+console.log(studenta);
+for(let a =0;a<studenta.length;a++){
+    console.log(studenta[a])
+
+}
+
+let studentb = [{name:"moosa"},{name:"khan"},{name:"jalil"}];
+console.log(studentb);
+for(let bb =0;bb<studentb.length;bb++){
+    console.log(studentb[bb].name)
+}
+let sc ={
+    name:"pop",
+    rollNumber: 0,
+    class: 9
+}
+console.log(sc.name);
+console.log(sc.class);
+console.log(sc.rollNumber);
+let studente;
+studente = ["moosa","khan","jalil","eijaz","noman"];
+
+let studenty;
+studenty = 5;
+console.log(studenty)
+
+let string = "this is MOOSA";
+console.log(string)
+sq ={
+    name:"pop",
+    rollNumber: 0,
+    class: 9
+}
+console.log(sq)
+
+let studentq = [true,false];
+
+let studentp = [true,false,"moosa","khan",12,13,15]
+
+let studento = ["moosa", "khan", "jalil"];
+let doo = [500, 500, 500];
+console.log(studento);
+console.log(doo);
+for (let i = 0; i < doo.length; i++) {
+  console.log(doo[i]);
+}
+
+let d = ["green", "white", "red"];
+for (let i = 0; i <d.length; i++) {
+   console.log(d[i]);
+}
+console.log(d)
+
+let g = []
+let j = [];
+let color =prompt("enter your color?")
+let m =prompt("enter your color?")
+let n =prompt("enter your color?")
+let colors =prompt("enter your color?")
+let user =prompt("user kaun sa number delete karna hai")
+console.log(color)
+ d.push(color)
+console.log(d)
+d.unshift(color)
+console.log(d)
+
+d.shift(colors)
+console.log(d)
+let e = d.shift()
+console.log(e)
+let s = d.pop()
+console.log(s)
+
+console.log(color)
+console.log(colors)
+
+g.push(color)
+g.push(colors)
+console.log(g)
+
+console.log(color)
+console.log(colors)
+console.log(user)
+console.log(m)
+console.log(n)
+j.push(color)
+j.push(colors)
+j.push(m)
+j.push(n)
+console.log(j)
+j.splice(3,1)
+console.log(j)
+
+let dr = [23,543,5432,234,543];
+dr.sort()
+console.log(dr)
+let cites = ["quetta","peshawar","IsIamabad","lahore","karachi"];
+console.log(cites)
+let w = cites.slice(1,3)
+console.log(w)
+
+var arr = ['this','is','my ','cat'];
+console.log(arr)
+let op = arr.join(" ")
+console.log(op)
+
+let pos =["Apple","Samsung"," Motorola", "Nokia", "Sony" ,"Haier"] 
+console.log(pos)
+let b = "  "
+for (let i = 0; i<pos.length ; i++ ){
+   b+=pos[i]
+}
+let k = document.getElementById("div")
+console.log(k)
+k.innerHTML=b    
